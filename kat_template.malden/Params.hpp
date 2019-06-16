092dedb1ff6560441d83370005d9273050413401
Katalam <goerss99@gmail.com>
2019-06-16 19:58:51 +0200

    Added params for night combat equipment

---
+++ kat_template.malden/Params.hpp
@@ -0,0 +1,8 @@
+class Params {
+    class GearLevel {
+        title = $STR_kat_template_nightvision;
+        texts[] = {$STR_kat_template_nightvision_0, $STR_kat_template_nightvision_1, $STR_kat_template_nightvision_2};
+        values[] = {0, 1, 2};
+        default = 0;
+    };
+};
