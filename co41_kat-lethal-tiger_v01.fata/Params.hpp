35870b619b17e6b8cfda2a6d614630b7ded7788f
Katalam <goerss99@gmail.com>
2019-08-22 17:48:43 +0200

    Removed co41_kat_lethal-tiger_v01.fata

--- co41_kat-lethal-tiger_v01.fata/Params.hpp
+++
@@ -1,8 +0,0 @@
-class Params {
-    class GearLevel {
-        title = $STR_kat_template_nightvision;
-        texts[] = {$STR_kat_template_nightvision_0, $STR_kat_template_nightvision_1, $STR_kat_template_nightvision_2};
-        values[] = {0, 1, 2};
-        default = 0;
-    };
-};
